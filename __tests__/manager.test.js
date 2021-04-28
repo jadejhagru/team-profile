e8515392c0320b825ff36c7139ce594a8ccbc0b6
Jade Jhagru <j.jhagru@hotmail.com>
2021-04-28 17:08:23 -0400

    tests

---
+++ __tests__/manager.test.js
@@ -0,0 +1,16 @@
+// using Manager constructor 
+const Manager = require('../lib/Manager');
+
+// creating manager object  
+test('creates an Manager object', () => {
+    const manager = new Manager('Jade', 90, 'j.jhagru@hotmail.com', 4);
+    
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const manager = new Manager('Jade', 90, 'j.jhagru@hotmail.com');
+
+    expect(manager.getRole()).toEqual("Manager");
+}); 
